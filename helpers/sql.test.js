186987bee9144b7093fc74918a3268701db8ce24
Mark <mark@example.dev>
2021-10-04 03:29:07 -0700

    implement SQL WHERE clause builder helper function

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,4 +1,4 @@
-const { sqlForPartialUpdate } = require('./sql');
+const { sqlForPartialUpdate, sqlForFilter } = require('./sql');
 const { BadRequestError } = require('../expressError');
 
 describe("sqlForPartialUpdate", function() {
@@ -7,7 +7,7 @@ describe("sqlForPartialUpdate", function() {
     data = {firstName: "John", age:40, lastName: "Doe"};
     definitions = {firstName: "first_name", lastName: "last_name"}
   })
-  test("returns expected output", function() {
+  test("works", function() {
     const result = sqlForPartialUpdate(data, definitions);
     expect(result.values).toEqual(["John", 40, "Doe"]);
     expect(result.setCols).toEqual('"first_name"=$1, "age"=$2, "last_name"=$3');
@@ -18,3 +18,16 @@ describe("sqlForPartialUpdate", function() {
     expect(s).toThrow(BadRequestError);
   });
 });
+
+describe("sqlForFilter", function() { 
+  test("works", function() {
+    const filters = [
+      {column: "name", operation: "ILIKE", value: "Dog"},
+      {column: "num_employees", operation: ">", value: 3},
+      {column: "num_employees", operation: "<", value: 6}];
+
+    const result = sqlForFilter(filters);
+    expect(result.values).toEqual(["%Dog%", 3, 6]);
+    expect(result.string).toEqual('"name" ILIKE $1 AND "num_employees" > $2 AND "num_employees" < $3');
+  })
+})
