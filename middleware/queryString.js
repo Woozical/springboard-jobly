601b33dcc015dead14d15d17b0438c6f316fb301
Mark <mark@example.dev>
2021-12-01 09:08:35 -0800

    bug fix, i love weak typing

--- middleware/queryString.js
+++ middleware/queryString.js
@@ -14,7 +14,7 @@ function cleanCompanyQString(req, res, next){
     if (req.query.name) params.name = req.query.name;
     if (req.query.minEmployees) params.minEmployees = req.query.minEmployees;
     if (req.query.maxEmployees) params.maxEmployees = req.query.maxEmployees;
-    if ((params.minEmployees && params.maxEmployees) && (params.minEmployees > params.maxEmployees)){
+    if ((params.minEmployees && params.maxEmployees) && (+params.minEmployees > +params.maxEmployees)){
       return next(new BadRequestError('Min employees can not be greater than max employees'));
     }
     if (Object.keys(params).length > 0) req.cleanQuery = {...params};
